c26a1fad07a4cdd16e8c35ebe9f6fd786996938b
Aaron Reeves <aaron.reeves@sruc.ac.uk>
2021-07-16 21:29:00 +0100

    License information added

--- cprogresswidget.cpp
+++ cprogresswidget.cpp
@@ -1,3 +1,17 @@
+/*
+cprogresswidget.h/cpp/ui
+------------------------
+Begin: 2021/07/05
+Author: Aaron Reeves <aaron.reeves@sruc.ac.uk>
+--------------------------------------------------
+Copyright (C) 2021 Epidemiology Research Unit, Scotland's Rural College (SRUC)
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
+Public License as published by the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+*/
+
+
 #include "cprogresswidget.h"
 #include "ui_cprogresswidget.h"
 
